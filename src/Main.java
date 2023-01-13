119b334f5e0e141501aff88403a9b21e3d023e20
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-13 08:37:27 +0700

    Design Triangle

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle1 = new Triangle(7.2, 9.6, 12.0, "green");
+        System.out.println(triangle1);
+
+        Triangle triangle2 = new Triangle(6.0, 8.0, 10.0, "blue");
+        System.out.println(triangle2);
+
+        Triangle triangle3 = new Triangle(30.0, 40.0, 50.0, "yellow");
+        System.out.println(triangle3);
+    }
+}
